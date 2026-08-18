2a78dcd00a3c717bfe72311dab148ce0623c454d
WY Developer <developer@wycleaning.nl>
2026-08-19 01:52:31 +0200

    deploy: placeholders voor eigen Hostinger account

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,12 +1,20 @@
 #!/bin/bash
 set -e
 
+# TODO: vul je eigen Hostinger SSH-gegevens in.
+# Dit is NIET hetzelfde account als waar carstorecuijk.nl op staat.
+SSH_KEY="/home/battletron/.ssh/JOUW_HOSTINGER_SSH_KEY"
+SSH_HOST="JOUW_HOSTINGER_IP"
+SSH_PORT="65002"
+SSH_USER="JOUW_HOSTINGER_USER"
+REMOTE_PATH="~/domains/carstoreparts.nl/public_html/"
+
 echo "Building carstoreparts.nl..."
 npm run build
 
-echo "Deploying to Hostinger..."
+echo "Deploying to Hostinger ($REMOTE_PATH)..."
 rsync -avz --progress \
-  -e "ssh -i /home/battletron/.ssh/carstorecuijk_deploy~ -p 65002 -o StrictHostKeyChecking=no" \
-  dist/ u258982067@194.36.187.37:~/domains/carstoreparts.nl/public_html/
+  -e "ssh -i $SSH_KEY -p $SSH_PORT -o StrictHostKeyChecking=no" \
+  dist/ "$SSH_USER@$SSH_HOST:$REMOTE_PATH"
 
 echo "Done."
